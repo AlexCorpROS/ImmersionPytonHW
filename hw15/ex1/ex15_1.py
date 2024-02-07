c45393e2d73398a73dc1860d32becd58b4c7a875
AlexCorpROS <arcorprintr@gmail.com>
2024-02-07 14:27:45 +0300

    Hw

--- hw15/ex1/ex15_1.py
+++ hw15/ex1/ex15_1.py
@@ -6,54 +6,66 @@
 ○ название родительского каталога.
 📌 В процессе сбора сохраните данные в текстовый файл используя логирование.'''
 
-import  argparse
-from collections import namedtuple
-import  os
-import logging
-
-FORMAT = ("{asctime} - {levelname}: {msg}")
-
-logging.basicConfig(filename='log/ex15_1.log', filemode='a', format=FORMAT, encoding='utf-8', style='{', level=logging.INFO)
-logger = logging.getLogger('main')
-
-parser = argparse.ArgumentParser(description="argument parser")
-parser.add_argument('-p', metavar='path', type= str or None , nargs='*', default=None, help='Укажите путь к директории или файлу')
-args = parser.parse_args()
-
-
-def split_path(lst_path: str):
-    fs_objects = []
-    if lst_path is None:
-        path_string = os.getcwd()
-        logger.warning(f'Пустой ввод.Путь установлен по умолчанию {path_string}')
-        abs_path, full_name = os.path.split(path_string)
-        abs_parent, parent = os.path.split(abs_path)
-        name = full_name
-        expansion = False
-        flag = True
-    else:
-        path_string = lst_path[0]
-        '''
-        Родительский каталог определяется на один уровень вверх.
-        При необходимости укзать абсолютный путь до файла/каталога вместо parent берем abs_path
-        '''
-        abs_path, full_name = os.path.split(path_string)
-        abs_parent, parent = os.path.split(abs_path)
-        if '.' in full_name:
-            name, expansion = full_name.split('.')
-            flag = False
-            logger.info(f'Выбран файл {full_name}.Абсолютный путь к файлу {abs_path}')
-        else:
-            name = full_name
-            expansion = False
-            flag = True
-            logger.info(f'Выбран каталог {full_name}. Абсолютный путь к каталогу {abs_path}')
-
-    Obj_info = namedtuple('Obj_info', 'name expansion catalog home', defaults=['', '', False, ''])
-    fs_objects.append(Obj_info(name=name, expansion=expansion, catalog=flag, home=parent))
-    return fs_objects
+def main():
+    pass
 
 if __name__ == '__main__':
-    print(split_path(args.p))
+    main()
+
+# import  argparse
+# from collections import namedtuple
+# import  os
+# import logging
+# import json
+#
+# FORMAT = ("{asctime} - {levelname}: {msg}")
+#
+# logging.basicConfig(filename='log/ex15_1.log', filemode='a', format=FORMAT, encoding='utf-8', style='{', level=logging.INFO)
+# logger = logging.getLogger('main')
+#
+# parser = argparse.ArgumentParser(description="argument parser")
+# parser.add_argument('-p', metavar='path', type= str or None , nargs='*', default=None, help='Укажите путь к директории или файлу')
+# args = parser.parse_args()
+#
+#
+# def split_path(lst_path: str):
+#     fs_objects = []
+#     if lst_path is None:
+#         path_string = os.getcwd()
+#         logger.warning(f'Пустой ввод.Путь установлен по умолчанию {path_string}')
+#         abs_path, full_name = os.path.split(path_string)
+#         abs_parent, parent = os.path.split(abs_path)
+#         name = full_name
+#         expansion = False
+#         flag = True
+#     else:
+#         path_string = lst_path[0]
+#         '''
+#         Родительский каталог определяется на один уровень вверх.
+#         При необходимости укзать абсолютный путь до файла/каталога вместо parent берем abs_path
+#         '''
+#         abs_path, full_name = os.path.split(path_string)
+#         abs_parent, parent = os.path.split(abs_path)
+#         if '.' in full_name:
+#             name, expansion = full_name.split('.')
+#             flag = False
+#             logger.info(f'Выбран файл {full_name}.Абсолютный путь к файлу {abs_path}')
+#         else:
+#             name = full_name
+#             expansion = False
+#             flag = True
+#             logger.info(f'Выбран каталог {full_name}. Абсолютный путь к каталогу {abs_path}.')
+#
+#     Obj_info = namedtuple('Obj_info', 'name expansion catalog home', defaults=['', '', False, ''])
+#     fs_objects.append(Obj_info(name=name, expansion=expansion, catalog=flag, home=parent))
+#     print(fs_objects[0])
+#     logger.info(f'Записаны данные {fs_objects}')  # Запись данных в логи
+#     with open('base.json', 'a') as file:  # Запись данных в файл
+#         json.dump(fs_objects, file)
+#         file.write('\n')
+#     return fs_objects
+#
+# if __name__ == '__main__':
+#     print(split_path(args.p))
 
 # pyton .\ex15_1.py -p C:\Work\PyCharm\PytoProject\venv\hw.exe
--- hw15/ex3/ex15_3.py
+++ hw15/ex3/ex15_3.py
@@ -5,36 +5,42 @@
 #
 # Внимание! Число 1 — не является ни простым, ни составным числом, так как у него только один делитель — 1.
 
-import logging
-import  argparse
-
-FORMAT = ("{asctime} - {levelname}: {msg}")
-logging.basicConfig(filename='log/ex15_3.log', filemode='a', format=FORMAT, encoding='utf-8', style='{', level=logging.INFO)
-logger = logging.getLogger('main')
-
-parser = argparse.ArgumentParser(description="argument parser")
-parser.add_argument('-num', metavar='number', type= int , default = 2, help='Введите целое число на проверку')
-args = parser.parse_args()
-
-def check_simple(num):
-    for i in range(2, int(num ** (1 / 2) + 1)):
-        if num % i == 0:
-            return False
-    return True
-
-def input_test(num):
-    if num < 2 or num > 100000:
-        print("Число должно быть больше 1 и меньше 100000")
-        logger.warning(f"Число должно быть больше 1 и меньше 100000. Вы ввели {num}")
-    else:
-        if check_simple(num) == True:
-            print(f'{num} является простым числом')
-            logger.info(f'Число {num} простое')
-        else:
-            print(f'{num} является составным числом')
-            logger.info(f'Число {num} составное')
+def main():
+    pass
 
 if __name__ == '__main__':
-    input_test(args.num)
+    main()
+
+# import logging
+# import  argparse
+#
+# FORMAT = ("{asctime} - {levelname}: {msg}")
+# logging.basicConfig(filename='log/ex15_3.log', filemode='a', format=FORMAT, encoding='utf-8', style='{', level=logging.INFO)
+# logger = logging.getLogger('main')
+#
+# parser = argparse.ArgumentParser(description="argument parser")
+# parser.add_argument('-num', metavar='number', type= int , default = 2, help='Введите целое число на проверку')
+# args = parser.parse_args()
+#
+# def check_simple(num):
+#     for i in range(2, int(num ** (1 / 2) + 1)):
+#         if num % i == 0:
+#             return False
+#     return True
+#
+# def input_test(num):
+#     if num < 2 or num > 100000:
+#         print("Число должно быть больше 1 и меньше 100000")
+#         logger.warning(f"Число должно быть больше 1 и меньше 100000. Вы ввели {num}")
+#     else:
+#         if check_simple(num) == True:
+#             print(f'{num} является простым числом')
+#             logger.info(f'Число {num} простое')
+#         else:
+#             print(f'{num} является составным числом')
+#             logger.info(f'Число {num} составное')
+#
+# if __name__ == '__main__':
+#     input_test(args.num)
 
 # pyton .\ex15_3.py -num 17
